c6dc861ad1f6769546c40cad76ab564813cef752
Mauricio Aliendre <mauricio.aliendre@unosquare.com>
2023-12-17 12:21:57 -0400

    Add the chrome dirver and change the BrowserTest.java file

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.File;
 
 
 public class BrowserTest {
@@ -17,9 +18,12 @@ public class BrowserTest {
 	public void getData()
 	{
 		System.out.println("Hello Guys");
-		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")
+				+ File.separator + "src" + File.separator + "test" + File.separator + "java" +
+				File.separator + "resources" + File.separator + "chromedriver.exe");
 		WebDriver driver =new ChromeDriver();
-		driver.get("https://rahylshettyacademy.azurewebsites.net/webapp/");
+		driver.get("http://localhost:8080/webapp/");
+//		driver.get("https://rahylshettyacademy.azurewebsites.net/webapp/");
 		String text =driver.findElement(By.cssSelector("h1")).getText();
 		System.out.println(text);
 		Assert.assertTrue(text.equalsIgnoreCase("RahulShettyAcademy.com Learning"));
